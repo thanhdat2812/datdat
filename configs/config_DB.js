c1936814851becba1abfc94deee81afc7bc6e069
Thanh Dat <datptgcc18100@fpt.edu.vn>
2021-07-02 22:00:34 +0700

    first commit

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb+srv://leducanh:leducanh@cluster0.uinee.mongodb.net/atnshop?retryWrites=true&w=majority',
+        dbname : "atnshop",
+        dbusername : "leducanh",
+        dbpassword : "anh123456",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://leducanh:anh123456@cluster0.uinee.mongodb.net/Shopacc?retryWrites=true&w=majority',
+        dbname : "atnshop",
+        dbusername : "leducanh",
+        dbpassword : "anh123456",
+    }
+
+};
---
+++ configs/config_Header.js
@@ -0,0 +1,10 @@
+module.exports = [
+    { name: "Home" , link: "/"},
+    { name: "Product" , link: "/product/view"},
+    { name: "Order" , link: "/order"},
+    { name: "Payment" , link: "/payment"},
+    { name: "Report" , link: "/chatting"},
+    { name: "Login" , link: "/login"},
+    { name: "create User" , link: "/user/create"},
+    { name: "QR code - link" , link: "/qr"},
+];
